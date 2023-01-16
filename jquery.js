73a3902ccc049cccf19651e92d67ff20da1543db
BangPenthol05 <102308710+BangPenthol05@users.noreply.github.com>
2023-01-16 21:01:31 +0700

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+	
+	var money = function(num) {
+                var str = parseFloat(num).toFixed(2);
+                var x = str.split('.');
+                x1 = x[0];
+                x2 = x.length > 1 ? '.' + x[1] : '';
+                var rgx = /(\d+)(\d{3})/;
+                while (rgx.test(x1)) {
+                    x1 = x1.replace(rgx, '$1' + ',' + '$2');
+                }
+                return x1 + x2;
+            }
+          $("#slider").slider({
+              animate: true,
+              value:1,
+              min: 0,
+              max: 1000000,
+              step: 100,
+              slide: function(event, ui) {
+                  update(1,ui.value); //changed
+              }
+          });
+
+          $("#slider2").slider({
+              animate: true,
+              value:0,
+              min: 1,
+              max: 500,
+              step: 1,
+              slide: function(event, ui) {
+                  update(2,ui.value); //changed
+              }
+          });
+
+          //Added, set initial value.
+          $("#amount").val(0);
+          $("#duration").val(0);
+          $("#amount-label").text(0);
+          $("#duration-label").text(1);
+          
+          update();
+      });
+
+      //changed. now with parameter
+      function update(slider,val) {
+        //changed. Now, directly take value from ui.value. if not set (initial, will use current value.)
+        var $amount = slider == 1?val:$("#amount").val();
+        var $duration = slider == 2?val:$("#duration").val();
+
+        /* commented
+        $amount = $( "#slider" ).slider( "value" );
+        $duration = $( "#slider2" ).slider( "value" );
+         */
+
+
+         $total = "Rp" + (50000 * $duration);
+				//var $totalpf = 19;
+				$totalpf = "Rp" + (4/100 * ($amount * $duration));
+         $( "#amount" ).val($amount);
+         $( "#amount-label" ).text($amount);
+         $( "#duration" ).val($duration);
+         $( "#duration-label" ).text($duration);
+         $( "#total" ).val($total);
+         $( "#total-label" ).text($total);
+				$( "#totalpf" ).val($totalpf);
+				$( "#totalpf-label" ).text($totalpf);
+
+         $('#slider a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$amount+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+         $('#slider2 a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$duration+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+      }
